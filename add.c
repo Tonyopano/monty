7327eaefdb40c156f769391d4da5fa660df6b1dc
Chizoba Chukwuoma <45069751+classychizzy@users.noreply.github.com>
2022-06-15 22:46:24 +0100

    removed whitespace

--- add.c
+++ add.c
@@ -5,7 +5,6 @@
  * @counter: line_number
  * Return: no return
 */
-
 void f_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
--- div.c
+++ div.c
@@ -5,6 +5,7 @@
  * @counter: line_number
  * Return: no return
 */
+
 void f_div(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
