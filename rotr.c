2f9904ff0bf8bad35a4c3d7c1f0497c7b33858da
Ayomide Suara <99369085+Aysuarex@users.noreply.github.com>
2022-06-15 06:47:31 -0700

    Update rotr.c

--- rotr.c
+++ rotr.c
@@ -1,32 +1,26 @@
 #include "monty.h"
-
 /**
-* rotr - rotates the last node of a stack_t stack
-* @stack: stack head
-* @line_count: line count
-*
-* Return: void
-*/
-void rotr(stack_t **stack, unsigned int line_count)
+  *f_rotr- rotates the stack to the bottom
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
-	stack_t *bottom;
-	stack_t *prev;
+	stack_t *copy;
 
-	(void) line_count;
-	if (!stack || !*stack || !(*stack)->next)
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
 		return;
-
-	bottom = *stack;
-
-	while (bottom->next)
-		bottom = bottom->next;
-
-	prev = bottom->prev;
-	bottom->next = *stack;
-	bottom->prev = NULL;
-	prev->next = NULL;
-	(*stack)->prev = bottom;
-	*stack = bottom;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
 }
-
-
